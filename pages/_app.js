276dfc86f9a007aa21fd479a74556afc9afdcacb
Finn Woelm <finn.woelm@unsdsn.org>
2020-09-11 11:41:04 +0200

    Set up materialUI theme
    Define a better set of font sizes for materialUI.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import App from "next/app";
 import Head from "next/head";
 import { useEffect } from "react";
-import { CssBaseline } from "@material-ui/core";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { TITLE } from "../config";
+import getTheme from "../helpers/getTheme";
 
 function MyApp({ Component, pageProps }) {
   // Remove the server-side injected CSS.
@@ -22,9 +23,11 @@ function MyApp({ Component, pageProps }) {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <Component {...pageProps} />
+      <ThemeProvider theme={getTheme()}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
