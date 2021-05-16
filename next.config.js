23c81d8ba5cc1d3b8204c2f39eac1780947b6b28
Finn Woelm <finn.woelm@unsdsn.org>
2021-05-16 16:15:08 +0200

    Webpack config: Use Webpack 5 syntax for IgnorePlugin
    From the migration notes for Webpack 5:
    - If you are using IgnorePlugin with a regular expression as argument,
    it takes an options object now:
    ```
    new IgnorePlugin({ resourceRegExp: /regExp/ })
    ```
    
    https://webpack.js.org/migrate/5/

--- next.config.js
+++ next.config.js
@@ -5,7 +5,11 @@ module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Do not bundle JSON data on client
     if (!isServer) {
-      config.plugins.push(new webpack.IgnorePlugin(/\/data\//));
+      config.plugins.push(
+        new webpack.IgnorePlugin({
+          resourceRegExp: /public\/data\/[^/]*\.json$/,
+        })
+      );
     }
 
     return config;
