0d9fd4c7f4bbf63831221d55b7429bc41b57fb99
Max <maxjgruber@gmail.com>
2021-11-05 11:22:06 +0100

    Remove emotion cache

--- pages/_app.js
+++ pages/_app.js
@@ -3,32 +3,16 @@ import Router from "next/router";
 import { useEffect } from "react";
 import { enableStaticRendering } from "mobx-react-lite";
 import { DefaultSeo } from "next-seo";
-import { CacheProvider } from "@emotion/react";
 import NextNProgress from "nextjs-progressbar";
 import { TITLE, META_DESCRIPTION, META_IMAGE, URL } from "root/config";
 import ThemeProvider from "components/ThemeProvider";
 import * as gtag from "helpers/gtag";
-import createEmotionCache from "helpers/createEmotionCache";
 
-const clientSideEmotionCache = createEmotionCache();
-
-const App = ({
-  Component,
-  pageProps,
-  emotionCache = clientSideEmotionCache,
-}) => {
+const App = ({ Component, pageProps }) => {
   // use static rendering in SSR mode
   if (typeof window === "undefined") {
     enableStaticRendering(true);
   }
-
-  // Remove the server-side injected CSS.
-  useEffect(() => {
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
 
   // Track pages
   useEffect(() => {
@@ -42,7 +26,7 @@ const App = ({
   }, []);
 
   return (
-    <CacheProvider value={emotionCache}>
+    <>
       <Head>
         <meta
           name="viewport"
@@ -59,7 +43,7 @@ const App = ({
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
-    </CacheProvider>
+    </>
   );
 };
 
