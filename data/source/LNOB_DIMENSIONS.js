24fe63072396c5e22a9f70ca7de9461a643dea3c
Max <maxjgruber@gmail.com>
2022-06-09 12:14:31 +0200

    Add hard coded LNOB dimensions

---
+++ data/source/LNOB_DIMENSIONS.js
@@ -0,0 +1,24 @@
+const LNOB_DIMENSIONS = [
+  {
+    id: 1,
+    category: "services",
+    label: "Accessibilité et qualité des services",
+  },
+  {
+    id: 2,
+    category: "pauvreté",
+    label: "Extrême pauvreté et privation matérielle",
+  },
+  {
+    id: 3,
+    category: "genre",
+    label: "Inégalité des sexes",
+  },
+  {
+    id: 4,
+    category: "revenu",
+    label: "Inégalité des revenus",
+  },
+];
+
+module.exports = { LNOB_DIMENSIONS };
