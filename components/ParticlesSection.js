a85b49b3491d6286f1fb0f39757d447d45e49655
SaraKasim <Sara.zaydoon96@gmail.com>
2022-06-14 11:07:58 +0200

    Update particle section to use particles

--- components/ParticlesSection.js
+++ components/ParticlesSection.js
@@ -1,6 +1,7 @@
 import { useMediaQuery } from "@mui/material";
 import { useTheme, styled } from "@mui/material/styles";
 import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 const StyledParticles = styled(Particles)({
   position: "absolute",
@@ -17,8 +18,16 @@ const ParticlesSection = () => {
   // which messes with styled particles.
   const isMobile = useMediaQuery(theme.breakpoints.only("xs"), { noSsr: true });
 
+  const particlesInit = async (main) => {
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
   return (
     <StyledParticles
+      init={particlesInit}
       options={{
         fullScreen: false,
         background: {
