38186ea04c17b8fb571e2fd06043e7cb6675429b
SaraKasim <Sara.zaydoon96@gmail.com>
2022-06-14 11:11:18 +0200

    Update footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,9 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Link, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import { Linkedin, Facebook, Twitter, EmailNewsletter } from "mdi-material-ui";
+import { REPORT_DOWNLOAD_URL } from "root/config";
 import contentSizeQuery from "helpers/contentSizeQuery";
+import { trackDownload } from "helpers/gtag";
 
 const FooterTypography = (props) => (
   <Typography color="#fff" variant="body2" {...props} />
@@ -24,34 +27,70 @@ ResponsiveGridItem.defaultProps = { item: true };
 
 const Logo = styled("img")({ height: 80 });
 
-const BoxWithShadow = styled(Box)`
-  box-shadow: 0px -2px 4px -1px rgba(0, 0, 0, 0.2),
-    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
-`;
+const BoxWithShadow = styled(Box)({
+  boxShadow: `0px -2px 4px -1px rgba(0, 0, 0, 0.2),
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)`,
+});
 
 const Footer = () => (
-  <BoxWithShadow
-    paddingY={4}
-    position="relative"
-    style={{ background: "#020722" }}
-  >
+  <BoxWithShadow paddingY={4} position="relative" bgcolor="#0073b0ff">
     <Container maxWidth="lg">
       <Grid container>
         <ResponsiveGridItem styled={{ width: 1 / 3 }}>
           <Logo
             alt="Logo"
             src="/static/logo.svg"
-            style={{ height: 80, marginBottom: 8 }}
+            style={{ height: 80, marginBottom: 10 }}
           />
+          <Typography variant="h6" color="white" gutterBottom>
+            Check us out on social media!
+          </Typography>
+          <Link href="https://www.linkedin.com/company/unsdsn/" target="_blank">
+            <Linkedin style={{ fontSize: "30px", color: "white" }} />
+          </Link>
+          <Link href="https://www.facebook.com/UNSDSN" target="_blank">
+            <Facebook style={{ fontSize: "30px", color: "white" }} />
+          </Link>
+          <Link href="https://twitter.com/UNSDSN" target="_blank">
+            <Twitter style={{ fontSize: "30px", color: "white" }} />
+          </Link>
+          <Link
+            href="https://us8.list-manage.com/subscribe?u=a04105bfca6c4cb8c24ff8680&id=2302100059"
+            target="_blank"
+          >
+            <EmailNewsletter style={{ fontSize: "30px", color: "white" }} />
+          </Link>
         </ResponsiveGridItem>
         <ResponsiveGridItem styled={{ width: 2 / 3 }}>
           <FooterTypography gutterBottom>
-            Nullam imperdiet nunc eros, quis laoreet nisi cursus at. Suspendisse
-            potenti. Vivamus sed metus non metus luctus placerat. Pellentesque
-            vel auctor lacus. Phasellus sit amet pellentesque lacus. Curabitur
-            accumsan interdum risus vitae condimentum. Maecenas at dui dolor.
-            Mauris at orci a lectus tristique efficitur ut ac sem. Nullam mattis
-            varius arcu, sed placerat nisl venenatis vel.
+            The Sustainable Development Report (formerly the SDG Index &
+            Dashboards) is a global assessment of countries&apos; progress
+            towards achieving the Sustainable Development Goals. It is a
+            complement to the official SDG indicators and the voluntary national
+            reviews.
+          </FooterTypography>
+          <FooterTypography gutterBottom>
+            All data presented on this website are based on the publication{" "}
+            <Link
+              color="inherit"
+              href={REPORT_DOWNLOAD_URL}
+              target="_blank"
+              onClick={() => trackDownload(REPORT_DOWNLOAD_URL)}
+            >
+              Sachs et al. (2022): From Crisis to Sustainable Development: the
+              SDGs as Roadmap to 2030 and Beyond. Sustainable Development Report
+              2022. Cambridge: Cambridge University Press.
+            </Link>
+          </FooterTypography>
+          <FooterTypography gutterBottom>
+            Feedback? Questions? Contact us at{" "}
+            <Link
+              color="inherit"
+              href="mailto:info@sdgindex.org"
+              target="_blank"
+            >
+              info@sdgindex.org
+            </Link>
           </FooterTypography>
         </ResponsiveGridItem>
       </Grid>
