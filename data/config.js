5a5d2b0560c999078b346dd9f2bb7254d1930eb0
Max <maxjgruber@gmail.com>
2022-06-14 15:23:57 +0200

    Update dataset

--- data/config.js
+++ data/config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   SOURCE_FILE_PATH: path.join(
     __dirname,
-    "./source/220608 Benin_indexdata.xlsx"
+    "./source/220613 Benin infranational.xlsx"
   ),
   DATA_SHEET: "Base complète",
   CODEBOOK_SHEET: "Codebook",
