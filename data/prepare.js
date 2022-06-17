6c133a8869dbaea8b68252bfd79c57fc4183d681
Max <maxjgruber@gmail.com>
2022-06-17 17:00:17 +0200

    Uncomment all set up functions

--- data/prepare.js
+++ data/prepare.js
@@ -1,14 +1,14 @@
 const { writeStoreToJson } = require("@sdgindex/data/parse");
-// const setUpAssessments = require("./setUpAssessments");
-// const setUpRegions = require("./setUpRegions");
-// const setUpObservations = require("./setUpObservations");
-// const setUpTimeseries = require("./setUpTimeseries");
+const setUpAssessments = require("./setUpAssessments");
+const setUpRegions = require("./setUpRegions");
+const setUpObservations = require("./setUpObservations");
+const setUpTimeseries = require("./setUpTimeseries");
 
 // Get data from source Excel files
-// setUpAssessments();
-// setUpRegions();
-// setUpObservations();
-// setUpTimeseries();
+setUpAssessments();
+setUpRegions();
+setUpObservations();
+setUpTimeseries();
 
 // Write data
 writeStoreToJson();
