66afbf407517877927197b962f633c93d0300ce2
SaraKasim <Sara.zaydoon96@gmail.com>
2022-06-24 10:42:59 +0200

    Switch colors order for dimensions legend

--- helpers/legendForScore.js
+++ helpers/legendForScore.js
@@ -1,11 +1,11 @@
 export const LEGEND = [
   {
-    color: "#36869a",
+    color: "#dbeef6",
     threshold: 60,
     label: "> 60",
   },
   {
-    color: "#4facc5",
+    color: "#b7dde7",
     threshold: 50,
     label: "50 - 60",
   },
@@ -15,12 +15,12 @@ export const LEGEND = [
     label: "40 - 50",
   },
   {
-    color: "#b7dde7",
+    color: "#4facc5",
     threshold: 30,
     label: "30 - 40",
   },
   {
-    color: "#dbeef6",
+    color: "#36869a",
     threshold: 0,
     label: "< 30",
   },
