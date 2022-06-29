a89d309bbdc03d4a20294d09c55fb7fad86e5f52
Max <maxjgruber@gmail.com>
2022-06-29 13:54:40 +0200

    Readjust legend

--- helpers/legendForScore.js
+++ helpers/legendForScore.js
@@ -1,21 +1,21 @@
 export const LEGEND = [
   {
-    color: "#36869a",
+    color: "#0D52BD",
     threshold: 65,
     label: "> 65",
   },
   {
-    color: "#4facc5",
+    color: "#0178c8",
     threshold: 50,
     label: "50 - 65",
   },
   {
-    color: "#94cddb",
+    color: "#7fbae4",
     threshold: 40,
     label: "40 - 50",
   },
   {
-    color: "#b7dde7",
+    color: "#84d9f8",
     threshold: 30,
     label: "30 - 40",
   },
