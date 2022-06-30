522ddc85b0d64e9db3ba0a9c8ae49793d4dba248
Max <maxjgruber@gmail.com>
2022-06-30 10:16:41 +0200

    Readjust legend thresholds (6)

--- helpers/legendForScore.js
+++ helpers/legendForScore.js
@@ -1,26 +1,31 @@
 export const LEGEND = [
   {
-    color: "#0D52BD",
-    threshold: 65,
-    label: "> 65",
+    color: "#0e4f95",
+    threshold: 70,
+    label: "> 70",
   },
   {
-    color: "#0178c8",
+    color: "#0092d3",
+    threshold: 60,
+    label: "60 - 70",
+  },
+  {
+    color: "#00A8FF",
     threshold: 50,
-    label: "50 - 65",
+    label: "50 - 60",
   },
   {
-    color: "#7fbae4",
+    color: "#84d9f8",
     threshold: 40,
     label: "40 - 50",
   },
   {
-    color: "#84d9f8",
+    color: "#b0dff8",
     threshold: 30,
     label: "30 - 40",
   },
   {
-    color: "#dbeef6",
+    color: "#dff1fd",
     threshold: 0,
     label: "< 30",
   },
@@ -38,3 +43,36 @@ export const getDepartmentFill = (department) => {
 
   return LEGEND.find((item) => score >= item.threshold).color;
 };
+
+// export const LEGEND = [
+//   {
+//     color: "#0D52BD",
+//     threshold: 60,
+//     label: "> 60",
+//   },
+//   {
+//     color: "#0178c8",
+//     threshold: 50,
+//     label: "50 - 60",
+//   },
+//   {
+//     color: "#7fbae4",
+//     threshold: 40,
+//     label: "40 - 50",
+//   },
+//   {
+//     color: "#84d9f8",
+//     threshold: 30,
+//     label: "30 - 40",
+//   },
+//   {
+//     color: "#beddf0",
+//     threshold: 0,
+//     label: "< 30",
+//   },
+//   {
+//     color: "#d8d8d8",
+//     fallback: true,
+//     label: "Information unavailable",
+//   },
+// ];
