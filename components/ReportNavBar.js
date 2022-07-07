9fa7d11c9cd4ba5419a28a66eeb9471f3b62ec70
SaraKasim <Sara.zaydoon96@gmail.com>
2022-07-07 14:23:30 +0200

    Change paths to french

--- components/ReportNavBar.js
+++ components/ReportNavBar.js
@@ -2,34 +2,34 @@ import { Breadcrumbs, Link } from "@mui/material";
 
 const ReportNavBar = () => (
   <Breadcrumbs separator="|">
-    <Link underline="hover" color="inherit" href="/chapters/executive-summary">
+    <Link underline="hover" color="inherit" href="/chapitres/executive-summary">
       Executive Summary
     </Link>
     <Link
       underline="hover"
       color="inherit"
-      href="/chapters/part-1-a-global-plan-to-finance-the-sdgs"
+      href="/chapitres/part-1-a-global-plan-to-finance-the-sdgs"
     >
       Part 1
     </Link>
     <Link
       underline="hover"
       color="inherit"
-      href="/chapters/part-2-the-sdg-index-and-dashboards"
+      href="/chapitres/part-2-the-sdg-index-and-dashboards"
     >
       Part 2
     </Link>
     <Link
       underline="hover"
       color="inherit"
-      href="/chapters/part-3-policy-efforts-and-commitments-for-the-sdgs"
+      href="/chapitres/part-3-policy-efforts-and-commitments-for-the-sdgs"
     >
       Part 3
     </Link>
     <Link
       underline="hover"
       color="inherit"
-      href="/chapters/part-4-sdg-data-systems-and-statistics"
+      href="/chapitres/part-4-sdg-data-systems-and-statistics"
     >
       Part 4
     </Link>
