2e854fb5f1aa0b53b2c014e2964161736e9eba8f
SaraKasim <Sara.zaydoon96@gmail.com>
2022-07-07 11:40:19 +0200

    JEST: Update tests with new data

--- tests/observations.test.js
+++ tests/observations.test.js
@@ -31,37 +31,37 @@ const findObservation = (regionSlug, assessmentId) => {
 // LNOB DIMENSION SCORES
 it("has correct SDG index score and rank for Alibori", () => {
   expect(findObservation("alibori", "LNOB1")).toMatchObject({
-    score: 18.95,
+    score: 19.97,
   });
 });
 
 it("has correct SDG index score and rank for Atlantique", () => {
   expect(findObservation("atlantique", "LNOB3")).toMatchObject({
-    score: 58.31,
+    score: 58.08,
   });
 });
 
 it("has correct SDG index score and rank for Mono", () => {
   expect(findObservation("mono", "LNOB4")).toMatchObject({
-    score: 49.24,
+    score: 49.46,
   });
 });
 
 it("has correct SDG index score and rank for Couffo", () => {
   expect(findObservation("couffo", "LNOB2")).toMatchObject({
-    score: 28.43,
+    score: 29.93,
   });
 });
 
 it("has correct SDG index score and rank for Zou", () => {
   expect(findObservation("zou", "LNOB3")).toMatchObject({
-    score: 52.27,
+    score: 52.23,
   });
 });
 
 it("has correct SDG index score and rank for Plateau", () => {
   expect(findObservation("plateau", "LNOB4")).toMatchObject({
-    score: 39.81,
+    score: 40.09,
   });
 });
 
@@ -123,8 +123,8 @@ it("has valid ratings", () => {
 
 /* Test The Total Number Of Extractions */
 it("has the correct number of observations for custom/goals/indicators", () => {
-  expect(Object.keys(store.observations)).toHaveLength(12 * (37 + 4));
+  expect(Object.keys(store.observations)).toHaveLength(12 * (36 + 4));
   // 12 = number of regions
-  // 37 = number of indicators
+  // 36 = number of indicators
   // 4 = lnob dimensions
 });
