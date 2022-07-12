32052744724fedab908ed6bdd9399b48b42c6679
SaraKasim <Sara.zaydoon96@gmail.com>
2022-07-12 15:12:49 +0200

    Add french executive summary

---
+++ pages/chapitres/resume-executif.js
@@ -0,0 +1,123 @@
+import ReportLayout from "layouts/ReportLayout";
+import ReportParagraph from "components/ReportParagraph";
+
+const ReportPart = () => (
+  <ReportLayout
+    color="#005a86"
+    subtitle="Summary of key findings and 
+    recommendations"
+    title="Résumé Exécutif"
+  >
+    <ReportParagraph>
+      A mi-chemin et conscient des d&eacute;fis &agrave; relever pour la
+      r&eacute;alisation des Objectifs de D&eacute;veloppement durable (ODD), le
+      Gouvernement du B&eacute;nin a sollicit&eacute; le R&eacute;seau de
+      solutions pour le d&eacute;veloppement durable des Nations Unies (SDSN)
+      afin de l&rsquo;accompagner dans le suivi et l&rsquo;&eacute;valuation
+      pour la mise en œuvre de l&rsquo;Agenda 2030. Ce premier rapport a pour
+      objectif de pr&eacute;senter l&rsquo;&eacute;tat des lieux et les
+      tendances des ODD du B&eacute;nin, ainsi qu&rsquo;une analyse des
+      politiques pour les atteindre &agrave; travers le cadre des &laquo; Six
+      Transformations &raquo; (Sachs et al, 2019). Il sert de
+      r&eacute;f&eacute;rence (&laquo; baseline &raquo;), &agrave; l&rsquo;aube
+      de la premi&egrave;re &eacute;mission obligatoire de l&rsquo;Eurobond ODD
+      par le Gouvernement b&eacute;ninois en juillet 2021 qui t&eacute;moigne de
+      l&rsquo;engagement fort pour acc&eacute;l&eacute;rer la mise en œuvre de
+      l&rsquo;Agenda 2030.
+    </ReportParagraph>
+    <ReportParagraph>
+      Il ressort de ce premier rapport les performances et tendances suivantes :
+    </ReportParagraph>
+    <ReportParagraph>
+      &bull; Le B&eacute;nin est &agrave; mi-chemin de la r&eacute;alisation des
+      ODD avec un score de 50,7 sur 100 pour l&rsquo;ensemble des 17 ODD.
+    </ReportParagraph>
+    <ReportParagraph>
+      &bull; Le B&eacute;nin se distingue du reste de la Communaut&eacute;
+      Economique des Etats de l&rsquo;Afrique de l&rsquo;Ouest (CEDEAO) avec des
+      progr&egrave;s depuis 2015 pour les ODD 2 (Faim &laquo; Z&eacute;ro
+      &raquo;), 8 (Travail d&eacute;cent et croissance &eacute;conomique), 9
+      (Industrie, Innovation et Infrastructure) et 14 (Vie aquatique) pour
+      lesquels la majorit&eacute; des pays de la sous- r&eacute;gion stagnent,
+      voire r&eacute;gressent.
+    </ReportParagraph>
+    <ReportParagraph>
+      &bull; Par rapport aux pays &agrave; revenu plus &eacute;lev&eacute;s, le
+      B&eacute;nin performe relativement bien sur les ODD 12 (Consommation et
+      production responsables) et 13 (Lutte contre le changement climatique).
+    </ReportParagraph>
+    <ReportParagraph>
+      &bull; Il y a des d&eacute;fis persistants pour la r&eacute;alisation de
+      la majorit&eacute; des ODD dans la r&eacute;gion. En particulier, les
+      tendances des ODD 4 (Education de qualit&eacute;), 10
+      (In&eacute;galit&eacute;s r&eacute;duites) et 11 (Villes et
+      Communaut&eacute;s durables) sont &agrave; surveiller.
+    </ReportParagraph>
+    <ReportParagraph>
+      &bull; &Agrave; l&rsquo;&eacute;chelle infranationale, l&rsquo;indice
+      &laquo; ne laisser personne de c&ocirc;t&eacute; &raquo; couvre quatre
+      dimensions de disparit&eacute;s : les in&eacute;galit&eacute;s
+      d&apos;acc&egrave;s aux services publics, l&rsquo;extr&ecirc;me
+      pauvret&eacute; et la privation mat&eacute;rielle, les
+      in&eacute;galit&eacute;s entre les sexes, et les in&eacute;galit&eacute;s
+      de revenu et de richesse. On constate des disparit&eacute;s entre les
+      r&eacute;gions du B&eacute;nin, les d&eacute;partements du sud proches de
+      la capitale &eacute;conomique (Cotonou) &eacute;tant plus favoris&eacute;s
+      et les d&eacute;fis li&eacute;s aux ODD &eacute;tant importants dans le
+      Nord.
+    </ReportParagraph>
+    <ReportParagraph>
+      Les donn&eacute;es utilis&eacute;es proviennent de sources internationales
+      en vue de faciliter les comparaisons avec les autres pays de la CEDEAO,
+      ainsi que de sources nationales pour l&rsquo;analyse infranationale. Comme
+      pour d&rsquo;autres pays en voie de d&eacute;veloppement, les
+      donn&eacute;es manquantes et les d&eacute;lais dans la production des
+      statistiques ne permettant pas d&rsquo;avoir la pleine mesure des
+      progr&egrave;s et efforts r&eacute;alis&eacute;s par le B&eacute;nin.
+      L&rsquo;analyse des efforts du gouvernement, en mati&egrave;re de
+      politiques publiques et d&rsquo;investissement, permet donc
+      d&rsquo;apporter des informations compl&eacute;mentaires pour
+      &eacute;valuer la performance du B&eacute;nin.
+    </ReportParagraph>
+    <ReportParagraph>
+      Le Rapport sur le D&eacute;veloppement Durable 2022 le souligne, le
+      B&eacute;nin est class&eacute; parmi les pays &agrave; &laquo; fort
+      engagement&nbsp;&raquo; en faveur des ODD selon notre analyse mondiale des
+      efforts gouvernementaux. L&rsquo;analyse du Programme d&rsquo;Action du
+      Gouvernement (PAG 2021-2026) &agrave; travers le cadre des &laquo; Six
+      Transformations &raquo; montre qu&rsquo;il cible de fa&ccedil;on
+      coh&eacute;rente les d&eacute;fis du B&eacute;nin pour atteindre les ODD.
+      L&rsquo;intensification des efforts du Gouvernement, &agrave;
+      l&rsquo;image de son programme d&rsquo;assurance pour le renforcement du
+      capital humain (ARCH) et son programme d&rsquo;acc&egrave;s universel
+      &agrave; l&rsquo;eau potable en milieu rural, permettront
+      d&rsquo;acc&eacute;l&eacute;rer la r&eacute;alisation de plusieurs ODD et
+      de &laquo; ne laisser personne de c&ocirc;t&eacute; &raquo; y compris dans
+      les r&eacute;gions plus d&eacute;favoris&eacute;es du pays.
+    </ReportParagraph>
+    <ReportParagraph>
+      De m&ecirc;me, l&rsquo;analyse du cadre institutionnel pour la mise en
+      œuvre de l&rsquo;Agenda 2030 au B&eacute;nin a permis de constater les
+      capacit&eacute;s institutionnelles dont le pays dispose pour la
+      r&eacute;alisation des ODD. Le B&eacute;nin a su s&rsquo;approprier les
+      ODD depuis 2016 afin de domestiquer les objectifs et adopter une
+      strat&eacute;gie de d&eacute;veloppement. Le pays dispose d&rsquo;un
+      appareil institutionnel transversal et d&rsquo;une volont&eacute;
+      politique forte qui pourraient lui permettre d&rsquo;obtenir
+      d&rsquo;importants r&eacute;sultats dans les ann&eacute;es &agrave; venir.
+    </ReportParagraph>
+    <ReportParagraph>
+      En outre, la r&eacute;alisation des ODD n&eacute;cessite des
+      investissements publics et priv&eacute;s &agrave; grande &eacute;chelle.
+      Le B&eacute;nin doit &ecirc;tre soutenu dans sa mobilisation
+      financi&egrave;re pour la r&eacute;alisation de l&rsquo;Agenda 2030.
+      L&rsquo;&eacute;mission de l&rsquo;Eurobond ODD par le Gouvernement du
+      B&eacute;nin en juillet 2021 constitue un tournant important dans
+      l&rsquo;engagement et les moyens mobilis&eacute;s pour la mise en œuvre de
+      l&rsquo;Agenda 2030.
+    </ReportParagraph>
+    <img src="/static/chapters/ODD.svg" />
+  </ReportLayout>
+);
+
+export default ReportPart;
