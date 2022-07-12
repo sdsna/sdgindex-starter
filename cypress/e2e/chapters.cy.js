48cd5e6e460a70fe5df81d4d2ced5ef36c071031
Max <maxjgruber@gmail.com>
2022-07-12 20:30:08 +0200

    Correct failing test

--- cypress/e2e/chapters.cy.js
+++ cypress/e2e/chapters.cy.js
@@ -5,7 +5,7 @@ describe("Chapters", () => {
   });
 
   it("contains all chapters", () => {
-    cy.get("a[href^='/chapitres/']").should("have.length", 4);
+    cy.get("a[href^='/chapitres/']").should("have.length", 5);
   });
 
   it("can load all chapters", () => {
