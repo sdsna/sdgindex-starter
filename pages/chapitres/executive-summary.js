e3efcce81e43b3171ef9d671da25b61fe1d08017
SaraKasim <Sara.zaydoon96@gmail.com>
2022-07-12 14:48:49 +0200

    Add english executive summary

--- pages/chapitres/executive-summary.js
+++ pages/chapitres/executive-summary.js
@@ -1,8 +1,5 @@
 import ReportLayout from "layouts/ReportLayout";
-import ReportFigure from "components/ReportFigure";
 import ReportParagraph from "components/ReportParagraph";
-import Emphasis from "components/Emphasis";
-import Italics from "components/Italics";
 
 const ReportPart = () => (
   <ReportLayout
@@ -12,184 +9,93 @@ const ReportPart = () => (
     title="Executive Summary"
   >
     <ReportParagraph>
-      <Emphasis>
-        Peace, diplomacy, and international cooperation are fundamental
-        conditions for the world to progress on the SDGs towards 2030 and
-        beyond.
-      </Emphasis>{" "}
-      The war in Ukraine and other military conflicts are humanitarian
-      tragedies. They also impact prosperity and social outcomes through the
-      rest of the world, including exacerbating poverty, food insecurity, and
-      access to affordable energy. The climate and biodiversity crises amplify
-      the impact of these crises. At the time of this writing in early May 2022,
-      the outcome of the war in Ukraine and other military conflicts, but also
-      of the health crisis, remain highly uncertain. Yet, it is clear that these
-      multiple and simultaneous crises have diverted policy attention and
-      priorities away from medium and long-term goals such as the SDGs and the
-      Paris Climate Agreement: a shift of focus towards short-term issues that
-      threatens to slow down or even stall the adoption of ambitious and
-      credible national and international plans but also squeezes available
-      international funding for sustainable development. Global cooperation and
-      commitment to the bedrock SDG principles of social inclusion, clean
-      energy, responsible consumption, and universal access to public services
-      are needed more than ever to respond to the major challenges of our times,
-      including security crises, pandemics, and climate change. Despite these
-      difficult times, the SDGs should remain the roadmap for achieving
-      sustainable development by 2030 and beyond.
+      At the halfway point, and aware of the remaining challenges to achieve the
+      Sustainable Development Goals (SDGs), the Government of Benin asked the UN
+      Sustainable Development Solutions Network (SDSN) to support it in the
+      monitoring and evaluation of the 2030 Agenda. This initial report presents
+      an evaluation of Benin&rsquo;s current performance and trends on the SDGs,
+      as well as an analysis of its policies to achieve them through
+      SDSN&rsquo;s &ldquo;Six Transformations&rdquo; framework (Sachs et al,
+      2019). This report serves as a baseline following the first issue of the
+      SDG Eurobond by the Government of Benin in July 2021, which demonstrates
+      their strong commitment to accelerate the implementation of the 2030
+      Agenda.
     </ReportParagraph>
     <ReportParagraph>
-      <Emphasis>
-        For the second year in a row, the world is no longer making progress on
-        the SDGs.
-      </Emphasis>{" "}
-      The average SDG Index score slightly declined in 2021, partly due to slow
-      or nonexistent recovery in poor and vulnerable countries. Multiple and
-      overlapping health and security crises have led to a reversal in SDG
-      progress. Performance on SDG&nbsp;1 (No&nbsp;Poverty) and SDG&nbsp;8
-      (Decent Work and Economic Growth) remains below pre-pandemic levels in
-      many low-income countries (LICs) and lower-middle-income countries
-      (LMICs). This is a major setback, especially considering that before the
-      pandemic, over the period 2015&ndash;2019, the world was progressing on
-      the SDGs at a rate of 0.5 points per year (which was also too slow to
-      reach the 2030 deadline), with poorer countries making greater gains than
-      rich countries. Progress on climate and biodiversity goals is also too
-      slow, especially in rich countries. Ahead of the heads of state SDG Summit
-      in 2023, restoring and accelerating SDG progress in all countries,
-      including the poorest and most vulnerable, should be a major priority of
-      recovery plans and reforms to the international development finance
-      system.
-    </ReportParagraph>
-    <ReportFigure
-      number="E.1"
-      title="SDG Index Score 
-      over time, 
-      world average
-      (2010-2021)"
-      note="Population-weighted average"
-      source="Authors' analysis"
-    />
-    <ReportParagraph>
-      <Emphasis>A global plan to finance the SDGs is needed.</Emphasis>{" "}
-      Achieving the SDGs is fundamentally an investment agenda in physical
-      infrastructure (including renewable energy) and human capital. Yet the
-      poorest half of the world &ndash; roughly speaking, the low-income
-      countries (LICs) and lower-middle-income countries (LMICs) &ndash; lacks
-      market access to capital on acceptable terms. We highlight five priorities
-      towards a global plan to finance the SDGs. <Emphasis>First</Emphasis>, the
-      G20 should declare clearly and unequivocally its commitment to channel far
-      larger flows of financing to developing countries so that they can achieve
-      economic development and meet the SDG targets. <Emphasis>Second</Emphasis>
-      , the G20 should greatly increase the lending capacity and annual flows of
-      the Multilateral Development Banks (MDBs), mainly through greater paid-in
-      capital to these institutions, but also through greater leverage of their
-      balance sheets. <Emphasis>Third</Emphasis>, the G20 should support other
-      measures as well &ndash; notably increased ODA, large-scale philanthropy,
-      and refinancing of debts falling due &ndash; to bolster SDG finance for
-      the LICs and LMICs. <Emphasis>Fourth</Emphasis>, the IMF and the
-      credit-rating agencies need to redesign the assessments of debt
-      sustainability, taking into account the growth potential of developing
-      countries and their need for far larger capital accumulation.{" "}
-      <Emphasis>Fifth</Emphasis>, working together with the IMF and the MDBs,
-      developing countries need to strengthen their debt management and
-      creditworthiness by integrating their borrowing policies with tax
-      policies, export policies, and liquidity management, all to prevent future
-      liquidity crises.
+      The key findings of this initial baseline report are the following:
     </ReportParagraph>
     <ReportParagraph>
-      <Emphasis>
-        At mid-point on the way to 2030, policy efforts and commitments
-        supporting the SDGs vary significantly across countries, including among
-        G20 countries.
-      </Emphasis>{" "}
-      Ambitious and sound national targets, strategies, and plans are crucial to
-      turning the SDGs into an action agenda. Every year, SDSN conducts a survey
-      of government efforts for the SDGs, to monitor how the goals are
-      integrated into official speeches, national plans, budgets and monitoring
-      systems. SDSN also compiles metrics to gauge the alignment of national
-      objectives and investments with the Six&nbsp;SDG Transformations. This
-      year&rsquo;s pilot score of{" "}
-      <Italics>Governments&rsquo; Commitment and Efforts for the SDGs</Italics>,
-      compiled for more than 60 countries, reveals that among G20 member states,
-      the United States, Brazil, and the Russian Federation exhibit the least
-      support for the 2030 Agenda and the SDGs. The United States is among the
-      few UN Member States to have never submitted a Voluntary National Review
-      (VNR). By contrast, Nordic countries demonstrate relatively high support
-      for the SDGs, as do Argentina, Germany, Japan and Mexico (all G20
-      countries). Some countries, such as Benin and Nigeria, for example, have
-      large gaps in their SDG Index yet also earn relatively high scores for
-      their policy efforts. This may help them achieve better results in coming
-      years. Interestingly, Benin and Mexico have both issued SDG Sovereign
-      Bonds in recent years to scale up their sustainable development
-      investments.
+      &bull; Benin is halfway to achieving the SDGs with a score of 50.7 out of
+      100 across all 17 SDGs.
     </ReportParagraph>
-    <ReportFigure
-      number="E.2"
-      title="Governments' Commitment and Efforts for the SDGs Score (pilot version) versus SDG 
-      Index Score"
-      note="G20 countries in red. The score for Ukraine reflects the situation as of January 2022."
-      source="Authors' analysis. Details on the methodology and the indicators used are available on www.sdgindex.org"
-    />
     <ReportParagraph>
-      <Emphasis>
-        Rich countries generate negative international spillovers notably
-        through unsustainable consumption; Europe is taking actions.
-      </Emphasis>{" "}
-      The 2022 SDG Index is topped by three Nordic countries &ndash; Finland,
-      Denmark and Sweden &ndash; and all top 10 countries are European
-      countries. Yet even these countries face major challenges in achieving
-      several SDGs. The 2022 International Spillover Index included in this
-      report underlines how rich countries, including many European countries,
-      generate negative socioeconomic and environmental spillovers, including
-      through unsustainable trade and supply chains. The European Union has
-      called for &lsquo;zero tolerance&rsquo; of child labor and has proposed
-      using trade to export European values throughout the world. Several
-      instruments and legislations are under discussion in the European Union to
-      address international spillover effects in the context of the European
-      Green Deal. At the member states&rsquo; level, in 2022, Sweden became the
-      first country to announce its intention to set a national target to curb
-      imported CO₂ emissions. Ahead of the 2023 SDG Summit, we underline four
-      major priorities to curb negative international spillovers generated by
-      rich countries: (1) Scale up international development and climate
-      finance; (2)&nbsp;Leverage technical cooperation and SDG diplomacy; (3)
-      Adopt national targets and instruments to address consumption-based
-      impacts on other countries (do no harm); (4) Strengthen monitoring and
-      data systems at international, national, industrial, and corporate levels
-      covering the full supply chains, and make them an integral part of SDG
-      reporting.
+      &bull; Benin stands out from the rest of the Economic Community of West
+      African States (ECOWAS) with progress on SDGs 2 (Zero hunger), 8 (Decent
+      work and economic growth), 9 (Industry, Innovation and Infrastructure) and
+      14 (Life below Water) since 2015, for which the majority of countries in
+      the sub-region are stagnating or even regressing.
     </ReportParagraph>
-    <ReportFigure
-      number="E.3"
-      title="SDG Index Score versus International Spillover Index Score"
-      source="Authors' analysis"
-    />
     <ReportParagraph>
-      <Emphasis>
-        The COVID-19 pandemic forced data providers to innovate and build new
-        forms of partnerships; these should be leveraged and scaled up to
-        promote SDG impacts by 2030 and beyond.
-      </Emphasis>{" "}
-      The pandemic led to a massive and sudden shift in demand for timely and
-      quality data to monitor the health impacts of COVID-19 and inform policy
-      intervention at international and national levels. The health situation
-      and lockdowns impacted traditional mechanisms for data collection,
-      including traditional face-to-face surveys. Data providers were pushed to
-      innovate and modernize their data collection methods and processes,
-      notably leveraging mobile and wireless technologies. This was accompanied
-      by a sharp acceleration in the use of non-traditional data sources,
-      including citizen science, social media, and earth observation data. New
-      dynamic dashboards, GIS instruments, and improved data visualizations and
-      infographics have facilitated a greater understanding of data and
-      statistics. Looking ahead, consolidating and scaling-up data innovations
-      and new forms of partnerships &ndash; including between the public and
-      private sectors (including technology providers) &ndash; while also
-      maintaining high standards for data quality and privacy, could help to
-      promote evidence-based SDG policies and interventions. More generally,
-      science, technological innovations, and data systems can help identify
-      solutions in times of crises and can provide decisive contributions to
-      address the major challenges of our times. These require increased and
-      prolonged investments in statistical capacities, R&amp;D, and education
-      and skills.
+      &bull; Compared to higher income countries, Benin performs relatively well
+      on SDGs 12 (Responsible Consumption and Production) and 13 (Climate
+      Action).
     </ReportParagraph>
+    <ReportParagraph>
+      &bull; There are persistent challenges to achieving the majority of the
+      SDGs in the region. In particular, the trends for SDGs 4 (Quality
+      Education), 10 (Reduced Inequalities) and 11 (Sustainable Cities and
+      Communities) must be monitored and reversed.
+    </ReportParagraph>
+    <ReportParagraph>
+      &bull; At the subnational level, the &ldquo;leave no one behind&rdquo;
+      index covers the following four dimensions: inequalities in access to
+      public services, extreme poverty and material deprivation, gender
+      inequalities, and inequalities of income and wealth. The index reveals
+      disparities between the regions of Benin &ndash; the southern departments
+      close to the economic capital (Cotonou) perform better, and significant
+      SDG challenges remain in the north.
+    </ReportParagraph>
+    <ReportParagraph>
+      The data used in these analyses come from international sources to
+      facilitate comparisons with other ECOWAS countries, as well as from
+      national sources for the subnational index. However, as in other
+      developing countries, missing data and delays in statistical production do
+      not allow timely and accurate measurement of the progress and efforts made
+      by Benin. Therefore, our analysis of the government&rsquo;s efforts, in
+      terms of public policies and investment, provides additional information
+      to assess Benin&rsquo;s performance.
+    </ReportParagraph>
+    <ReportParagraph>
+      The 2022 Sustainable Development Report ranks Benin among the countries
+      with &ldquo;strong commitment&rdquo; to the SDGs according to our global
+      survey of government efforts. The analysis of the Government&rsquo;s
+      Action Program (PAG 2021- 2026) through the framework of the &ldquo;Six
+      Transformations&rdquo; shows that the PAG coherently targets Benin&rsquo;s
+      challenges in achieving the SDGs. The intensification of the
+      government&rsquo;s efforts, such as the insurance program for the
+      reinforcement of human capital (ARCH) and its program to ensure universal
+      access to drinking water in rural areas, will make it possible to
+      accelerate the achievement of several SDGs and to &ldquo;leave no one
+      behind&rdquo;, including in the most disadvantaged regions of the country.
+    </ReportParagraph>
+    <ReportParagraph>
+      Similarly, the analysis of the institutional framework for the
+      implementation of the 2030 Agenda in Benin has revealed the strong
+      institutional capacities for the achievement of the SDGs. Since 2016,
+      Benin has appropriated the SDGs to domesticate the goals and adopt a
+      coherent development strategy. The country has a cross-cutting
+      institutional apparatus and strong political will that could enable it to
+      achieve significant results in the years to come.
+    </ReportParagraph>
+    <ReportParagraph>
+      Furthermore, achieving the SDGs requires large-scale public and private
+      investments. Benin must be supported in its resource mobilization to
+      achieve the 2030 Agenda. The issuance of the SDG Eurobond by the
+      Government of Benin in July 2021 constitutes an important turning point in
+      the commitment and means mobilized for the implementation of the 2030
+      Agenda.
+    </ReportParagraph>
+    <img src="/static/chapters/SDG.svg" />
   </ReportLayout>
 );
 
