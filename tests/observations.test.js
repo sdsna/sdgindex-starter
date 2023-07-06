16782041c34ed9f686bba3c1be7bb099390da152
Max <maxjgruber@gmail.com>
2023-07-06 15:15:45 +0200

    JEST: Add ratings and trends to observations tests

--- tests/observations.test.js
+++ tests/observations.test.js
@@ -7,7 +7,12 @@ import {
   loadData,
   store,
 } from "@sdgindex/data";
-import { getRating, getValue, getYear } from "@sdgindex/data/observations";
+import {
+  getRating,
+  getTrend,
+  getValue,
+  getYear,
+} from "@sdgindex/data/observations";
 
 beforeAll(async () => {
   await loadData();
@@ -24,91 +29,113 @@ const findObservation = (regionSlug, assessmentId) => {
     ...observation,
     rating: getRating(observation),
     value: getValue(observation),
+    trend: getTrend(observation),
     year: getYear(observation),
   };
 };
 
 // LNOB DIMENSION SCORES
-it("has correct SDG index score and rank for Alibori", () => {
+it("has correct SDG index score, rating and trend for Alibori", () => {
   expect(findObservation("alibori", "LNOB1")).toMatchObject({
     score: 26.55,
+    rating: "red",
+    trend: "➚",
   });
 });
 
-it("has correct SDG index score and rank for Atlantique", () => {
+it("has correct SDG index score, rating and trend for Atlantique", () => {
   expect(findObservation("atlantique", "LNOB3")).toMatchObject({
     score: 59.91,
+    rating: "red",
+    trend: "➚",
   });
 });
 
-it("has correct SDG index score and rank for Mono", () => {
+it("has correct SDG index score, rating and trend for Mono", () => {
   expect(findObservation("mono", "LNOB4")).toMatchObject({
     score: 68.98,
+    rating: "orange",
+    trend: "•",
   });
 });
 
-it("has correct SDG index score and rank for Couffo", () => {
+it("has correct SDG index score, rating and trend for Couffo", () => {
   expect(findObservation("couffo", "LNOB2")).toMatchObject({
     score: 38.74,
+    rating: "red",
+    trend: "→",
   });
 });
 
-it("has correct SDG index score and rank for Zou", () => {
-  expect(findObservation("zou", "LNOB3")).toMatchObject({
-    score: 48.16,
+it("has correct SDG index score, rating and trend for Ouémé", () => {
+  expect(findObservation("oueme", "LNOB4")).toMatchObject({
+    score: 93.88,
+    rating: "yellow",
+    trend: "•",
   });
 });
 
-it("has correct SDG index score and rank for Plateau", () => {
-  expect(findObservation("plateau", "LNOB4")).toMatchObject({
-    score: 64.16,
+it("has correct SDG index score, rating and trend for Plateau", () => {
+  expect(findObservation("plateau", "LNOB1")).toMatchObject({
+    score: 35.25,
+    rating: "red",
+    trend: "→",
   });
 });
 
 // INDICATORS
 
-it("has correct indicator data for Ouémé", () => {
+it("has correct indicator data (value, rating, trend) for Ouémé", () => {
   expect(findObservation("oueme", "lnb1_accouch")).toMatchObject({
     value: 94.33,
     rating: "yellow",
+    trend: "↑",
   });
   expect(findObservation("oueme", "lnb1_eau")).toMatchObject({
     value: 80.75,
     rating: "yellow",
+    trend: "→",
   });
   expect(findObservation("oueme", "lnb3_scolamparit")).toMatchObject({
     value: 0.63,
     rating: "orange",
+    trend: "↑",
   });
 });
 
-it("has correct indicator data for the Donga", () => {
+it("has correct indicator data (value, rating, trend) for the Donga", () => {
   expect(findObservation("donga", "lnb3_mariage")).toMatchObject({
     value: 32.77,
     rating: "orange",
+    trend: "↓",
   });
   expect(findObservation("donga", "lnb4_gini")).toMatchObject({
     value: 0.32,
     rating: "yellow",
+    trend: "•",
   });
   expect(findObservation("donga", "lnb2_malnutri")).toMatchObject({
     value: 11.7,
     rating: "orange",
+    trend: "↓",
   });
 });
 
-it("has correct indicator data for the Collines", () => {
+it("has correct indicator data (value, rating, trend) for the Collines", () => {
   expect(findObservation("collines", "lnb3_intparit")).toMatchObject({
     value: 0.28,
     rating: "red",
+    trend: "•",
   });
   expect(findObservation("collines", "lnb4_revmoy")).toMatchObject({
     value: 17.56,
     rating: "yellow",
+    trend: "•",
   });
   expect(findObservation("collines", "lnb3_povparit")).toMatchObject({
     value: 0.96,
     rating: "green",
+    trend: "•",
   });
 });
 
