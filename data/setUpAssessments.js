c685367c21301879ff6ba2fd121e184b4e904203
Max <maxjgruber@gmail.com>
2023-07-06 12:41:47 +0200

    Revert "Set description and reference to null"
    This reverts commit 7558d192e3845c94b3d8435219f7f3be52a51e11.

--- data/setUpAssessments.js
+++ data/setUpAssessments.js
@@ -33,9 +33,7 @@ const setUpAssessments = () => {
       id: row.IndCode,
       goalNumber: row.cat_lnob,
       // NOTE: description is missing in the excel file
-      // As GSP transforms our data into JSON (and json doesn't accept undefined values),
-      // we temporarily need to set missing description to "null" (decription pending)
-      description: row.Description ? row.Description : null,
+      description: row.Description,
       labelWithUnit: row["Indicateur"].trim(),
       longTermObjective: roundNumber(row["Valeur Cible"], 2),
       longTermObjectiveReason: row["bestjust_FR"].trim(),
@@ -44,9 +42,7 @@ const setUpAssessments = () => {
       year: row["Reference_Year"],
       source: row.Source.trim(),
       // NOTE: we are missing reference in the excel file
-      // As GSP transforms our data into JSON (and json doesn't accept undefined values),
-      // we temporarily need to set missing description to "null" (decription pending)
-      reference: row["Reference"] ? row["Reference"] : null,
+      reference: row["Reference"],
     });
   });
 };
