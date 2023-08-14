32b243082d37f2f6f6d36e770cf6d1a46071a386
Max <maxjgruber@gmail.com>
2023-08-14 17:12:23 +0200

    Remove unused local helpers

--- helpers/getIndicatorsByDimension.js
+++
@@ -1,6 +0,0 @@
-import { getIndicators } from "@sdgindex/data";
-
-export const getIndicatorsByDimension = (dimension) =>
-  getIndicators().filter(
-    (indicator) => indicator.goalNumber === dimension.category
-  );
